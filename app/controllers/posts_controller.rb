3a5311d2128fabf67d09caf72db4b7c62e0495d1
sandralouise <sandra.adamshallie@gmail.com>
2016-05-22 19:09:14 +0000

    Update post controller and add post model. Also add index views and posts partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   end
   
   def index
+    @posts = Post.paginate(page: params[:page], per_page: 15).order('created_at DESC')
   end
 end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+
+  def timestamp
+    created_at.strftime('%d %B %Y %H:%M:%S')
+  end
+  
+  self.per_page = 10
+  
 end
